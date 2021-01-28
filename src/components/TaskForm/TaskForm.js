c20370d0ada733a8ca1590316ed03242546b21bc
Evy1 <evihysa0@gmail.com>
2021-01-28 17:43:11 +0100

    fixes on get/post method firebase

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,76 +1,106 @@
-import React,{useState} from 'react';
-import {Form, Button, Container} from 'react-bootstrap';
-import results from '../results';
-import {Link} from 'react-router-dom';
-import Dashboard from '../Dashboard';
-import styles from './TaskForm.module.css';
-import {useAuth} from '../../context/AuthContext';
+import React, { useState } from "react";
+import { Form, Button, Container } from "react-bootstrap";
+import results from "../results";
+import { Link } from "react-router-dom";
+import Dashboard from "../Dashboard";
+import styles from "./TaskForm.module.css";
+import { useAuth } from "../../context/AuthContext";
+import firebase from "../../firebase";
+import { useFormik } from 'formik';
 
-function TaskForm(){
+function TaskForm() {
+  const [title, setTitle] = useState();
+  const [content, setContent] = useState();
+  const [date, setDate] = useState();
+  const { currentUser } = useAuth();
+  const [message, setMessage] = useState();
+  const [error, setError] = useState();
 
-    const [title, setTitle] = useState();
-    const [content, setContent] = useState();
-    const [date, setDate] = useState();
-    const {currentUser} = useAuth();
-    const [message, setMessage] = useState();
-    const [error, setError] = useState();
-
-  const postHandler = e =>{
+  const postHandler = (e) => {
     e.preventDefault();
-    const data = { 
-            "title": title,
-            "content": content,
-            "date": date,
-            "user": currentUser.uid,
-            "status": 'pending'
-    }
-    results.post('/task.json', data).then(response=>{
-        setMessage('Task was recorded');
-        setTitle('')
-        setContent('')
-        setDate('')
-    }).catch(error=>{
-        setError('Task couldnt register ')
-    })
-  }
-    return(
-            <>
-            <Dashboard/>
-            <Container>
-                <div className="row py-4 justify-content-center">
-                    <h2>Add a new task here</h2>
-                </div>
-                <div className="row">
-                    <form onSubmit={postHandler} className={styles.Form}>
-                        {message && 
-                        <div className={styles.Success}>
-                            {message}
-                        </div> }
-                        {error && 
-                        <div className={styles.Failed}>
-                            {error}
-                        </div>}
-                        <Form.Group id="title" className="d-flex justify-content-center flex-column text-center">
-                            <label>Task Title</label>
-                            <input className={styles.Input} type="text"  required placeholder="Title" value={[title]} onChange={(e)=> setTitle(e.target.value)}/>
-                        </Form.Group>
-                        <Form.Group id="content" className="d-flex justify-content-center flex-column text-center">
-                            <label>Task Content</label>
-                            <textarea className={styles.Input} type="textarea" rows={3} required placeholder="Task" value={[content]} onChange={(e)=> setContent( e.target.value)}/>
-                        </Form.Group>
-                        <Form.Group id="date" className="d-flex justify-content-center flex-column text-center">
-                            <label>Pick a deadline</label>
-                            <input className={styles.Input} type="datetime-local" value={[date]} onChange={(e)=> setDate(e.target.value)}></input>
-                        </Form.Group>
-                        <div>
-                            <Button type="submit">Submit</Button>
-                            <Link className="btn-primary p-2 ml-4" to="/">Cancel</Link>
-                        </div>
-                    </form>
-                </div>
-            </Container>
-            </>
-       )
-    
+    const data = {
+      title: title,
+      content: content,
+      date: date,
+      user: currentUser.uid,
+      status: "pending",
+    };
+    firebase
+      .database()
+      .ref('task')
+      .push(data)
+      .then(() => {
+        setMessage("Task was recorded");
+        setTitle("");
+        setContent("");
+        setDate("");
+      })
+      .catch((error) => {
+        setError("Task couldnt register ");
+      });
+  };
+  return (
+    <>
+      <Dashboard />
+      <Container>
+        <div className="row py-4 justify-content-center">
+          <h2>Add a new task here</h2>
+        </div>
+        <div className="row">
+          <form onSubmit={postHandler} className={styles.Form}>
+            {message && <div className={styles.Success}>{message}</div>}
+            {error && <div className={styles.Failed}>{error}</div>}
+            <Form.Group
+              id="title"
+              className="d-flex justify-content-center flex-column text-center"
+            >
+              <label>Task Title</label>
+              <input
+                className={styles.Input}
+                type="text"
+                required
+                placeholder="Title"
+                value={[title]}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group
+              id="content"
+              className="d-flex justify-content-center flex-column text-center"
+            >
+              <label>Task Content</label>
+              <textarea
+                className={styles.Input}
+                type="textarea"
+                rows={3}
+                required
+                placeholder="Task"
+                value={[content]}
+                onChange={(e) => setContent(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group
+              id="date"
+              className="d-flex justify-content-center flex-column text-center"
+            >
+              <label>Pick a deadline</label>
+              <input
+                className={styles.Input}
+                type="datetime-local"
+                value={[date]}
+                onChange={(e) => setDate(e.target.value)}
+              ></input>
+            </Form.Group>
+            <div>
+              <Button type="submit">Submit</Button>
+              <Link className="btn-primary p-2 ml-4" to="/">
+                Cancel
+              </Link>
+            </div>
+          </form>
+        </div>
+      </Container>
+    </>
+  );
 }
 export default TaskForm;
--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,76 +1,101 @@
-import React,{useState, useEffect} from 'react';
-import {Card, Container} from 'react-bootstrap';
-import styles from './Tasks.module.css';
-import {useAuth} from '../../context/AuthContext'
-import results from '../results';
-import Buttons from '../Buttons/Buttons';
-import firebase from '../../firebase';
-import Spinner from '../Spiner/Spiner';
+import React, { useState, useEffect } from "react";
+import { Card, Container } from "react-bootstrap";
+import styles from "./Tasks.module.css";
+import { useAuth } from "../../context/AuthContext";
+// import results from "../results";
+import Buttons from "../Buttons/Buttons";
+import firebase from "../../firebase";
+import Spinner from "../Spiner/Spiner";
 
- function Tasks(){
-     const [items, setItems] = useState([]);
-     const {currentUser} = useAuth();
-     const [loading, setLoading] = useState(true);
-  
-    useEffect(()=>{
-        const abortController = new AbortController();
-        const signal = abortController.signal
-        results.get('/task.json',{signal : signal}).then(response=>{
-            const fetchedResults = [];   
-                for(let key in response.data){
-                    fetchedResults.unshift({
-                        ...response.data[key],
-                        id: key,
-                    }
-                    )
-                }
-            setItems(fetchedResults)
-            setLoading(false)
-        }) 
-        return function cleanUp(){
-            abortController.abort()
+function Tasks() {
+  const [items, setItems] = useState([]);
+  const { currentUser } = useAuth();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = firebase.database().ref("task").on("value", (snapshot) => {
+        const fetchedResults = [];
+        if(snapshot){
+            snapshot.forEach(snap => {
+                fetchedResults.push({
+                    ...snap.val(),
+                    id: snap.key,
+                })
+            })
+            setItems(fetchedResults);
+            setLoading(false);
+        }else{
+            return;
         }
-     },[items])
+    })
+    // const abortController = new AbortController();
+    // const signal = abortController.signal;
+    // results.get("/task.json", { signal: signal }).then((response) => {
+    //   const fetchedResults = [];
+    //   for (let key in response.data) {
+    //     fetchedResults.unshift({
+    //       ...response.data[key],
+    //       id: key,
+    //     });
+    //   }
+    //   setItems(fetchedResults);
+    //   setLoading(false);
+    // });
+    // return function cleanUp() {
+    //   abortController.abort();
+    // };
+    return unsubscribe;
+  }, []);
 
+  function deleteItem(id) {
+    const db = firebase.database().ref("task/" + id);
+    db.remove();
+  }
 
-     function deleteItem(id){ 
-        const db = firebase.database().ref('task/' + id);
-        db.remove()
-        setItems(items)
-     }
-     
-     let info = <Spinner/>;
-     if(!loading){
-         info = <div className="row d-flex justify-content-center text-center pt-4">
-                    {items.map(result=>(
-                        result.user === currentUser.uid &&
-                        <div className="col-12 col-md-4" key={result.id}>
-                        <Card className={styles.Card} >
-                        <Card.Body>
-                            <div>
-                                {result.date}
-                            </div>
-                            <h2 className={styles.TaskTitle}>{result.title}</h2>
-                            <div>
-                                <p>{result.content}</p>
-                            </div>
-                            <div className="d-flex justify-content-center">
-                                <Buttons  click={ () => firebase.database().ref('task/' + result.id).update({status: 'done'}).then(setItems(items))} 
-                                class={styles.DoneBtn} disable={result.status === "done"}>Done</Buttons>
-                                <Buttons  click={() => deleteItem(result.id) } 
-                                class={styles.DeleteBtn}>Delete</Buttons>
-                                <Buttons  class={styles.EditBtn}>Edit</Buttons>
-                            </div>
-                        </Card.Body>
-                    </Card>
+  let info = <Spinner />;
+  if (!loading) {
+    info = (
+      <div className="row d-flex justify-content-center text-center pt-4">
+        {items.map(
+          (result) =>
+            result.user === currentUser.uid && (
+              <div className="col-12 col-md-4" key={result.id}>
+                <Card className={styles.Card}>
+                  <Card.Body>
+                    <div>{result.date}</div>
+                    <h2 className={styles.TaskTitle}>{result.title}</h2>
+                    <div>
+                      <p>{result.content}</p>
                     </div>
-                    )) }
-                </div>
-                }
-        return (
-            <Container>
-                {info}
-            </Container>
-        )
- }
+                    <div className="d-flex justify-content-center">
+                      <Buttons
+                        click={() =>
+                          firebase
+                            .database()
+                            .ref("task/" + result.id)
+                            .update({ status: "done" })
+                        }
+                        class={styles.DoneBtn}
+                        disable={result.status === "done"}
+                      >
+                        Done
+                      </Buttons>
+                      <Buttons
+                        click={() => deleteItem(result.id)}
+                        class={styles.DeleteBtn}
+                      >
+                        Delete
+                      </Buttons>
+                      <Buttons class={styles.EditBtn}>Edit</Buttons>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </div>
+            )
+        )}
+      </div>
+    );
+  }
+  return <Container>{info}</Container>;
+}
 export default Tasks;
