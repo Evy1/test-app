263df1663dd68c8176a9c4e2a77e3f012522bcdc
Evy1 <evihysa0@gmail.com>
2021-02-01 17:11:46 +0100

    edit action

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -5,14 +5,33 @@ import { useAuth } from "../../context/AuthContext";
 import Buttons from "../Buttons/Buttons";
 import firebase from "../../firebase";
 import Spinner from "../Spiner/Spiner";
+import classes from "./Modal.module.css";
 
 function Tasks() {
   const [items, setItems] = useState([]);
   const { currentUser } = useAuth();
   const [loading, setLoading] = useState(true);
   const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [value, setValue] = useState([]);
+  const [content, setContent] = useState([]);
+  const [modal, setModal] = useState([]);
+  const [date, setDate] = useState();
+  const [touched, setTouched] = useState(false)
+  const buttonRef = useRef(null);
+  const handleClose = () => {
+    setShow(false);
+    setTouched(false)
+  }
+
+  //showing modal with the current content to edit
+  //data is pased from result in items iteration
+  const handleShow = (data) => {
+    setShow(true);
+    setModal(data);
+    setValue(data.title);
+    setContent(data.content);
+    setDate(data.date);
+  };
 
   useEffect(() => {
     const unsubscribe = firebase
@@ -36,6 +55,34 @@ function Tasks() {
     return unsubscribe;
   }, []);
 
+
+  //edit method
+  //id passed from modal id
+  const editTaskHandler = (id) => {
+    firebase
+      .database()
+      .ref("task/" + id)
+      .update({ title: value, content: content, date: date });
+  };
+  //handle change in title
+  const titleHandler= (e) =>{
+    e.preventDefault();
+    setValue(e.target.value);
+    setTouched(true)
+  }
+  //handle change in content of task
+  const contentHandler = (e) =>{
+    e.preventDefault();
+    setContent(e.target.value);
+    setTouched(true)
+  } 
+  //handle change in date
+  const dateHandler = (e)=>{
+    e.preventDefault()
+    setDate(e.target.value)
+    setTouched(true)
+  }
+  //delete method
   function deleteItem(id) {
     const db = firebase.database().ref("task/" + id);
     db.remove();
@@ -75,25 +122,72 @@ function Tasks() {
                       >
                         Delete
                       </Buttons>
-                      <Buttons class={styles.EditBtn} click={handleShow}>
+                      <Buttons
+                        class={styles.EditBtn}
+                        ref={buttonRef.current}
+                        click={() => handleShow(result)}
+                      >
                         Edit
                       </Buttons>
                     </div>
                   </Card.Body>
                 </Card>
-                <Modal show={show} onHide={handleClose} key={result.id}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>{result.content}</Modal.Title>
+                <Modal show={show} onHide={handleClose} animation={false}>
+                  <Modal.Header closeButton className={classes.Header}>
+                    <Modal.Title className={classes.Title}>
+                      Edit Task
+                    </Modal.Title>
                   </Modal.Header>
-                  <Modal.Body>{result.content}</Modal.Body>
-                  <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                      Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                      Save Changes
-                    </Button>
-                  </Modal.Footer>
+                  <Modal.Body className={classes.Body}>
+                    <form
+                      onSubmit={() => editTaskHandler(modal.id)}
+                      className="d-flex justify-content-center flex-column"
+                    >
+                      <label className={classes.Label}>Task Title</label>
+                      <input
+                        className={classes.Input}
+                        type="text"
+                        required
+                        placeholder="Enter the new title..."
+                        value={value}
+                        ref={buttonRef.current}
+                        onChange={titleHandler}
+                      ></input>
+                      <label className={classes.Label}>Task Content</label>
+                      <textarea
+                        className={classes.Input}
+                        type="textarea"
+                        rows={3}
+                        required
+                        placeholder="Enter new description..."
+                        value={[content]}
+                        onChange={contentHandler}
+                      />
+                      <label className={classes.Label}>Task Deadline</label>
+                      <input
+                        className={classes.Input}
+                        type="datetime-local"
+                        value={[date]}
+                        onChange={dateHandler}
+                      ></input>
+                      <Button
+                        type="submit"
+                        variant="primary"
+                        className={classes.Button}
+                        disabled = {!touched}
+                      >
+                        Save Changes
+                      </Button>
+                      <Button
+                        className={classes.CancelBtn}
+                        variant="secondary"
+                        onClick={handleClose}
+                      >
+                        Cancel
+                      </Button>
+                    </form>
+                  </Modal.Body>
+                  <Modal.Footer className={classes.Body}></Modal.Footer>
                 </Modal>
               </div>
             )
